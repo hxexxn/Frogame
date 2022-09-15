2b42761bdcbc01bdc24104902896c12fb45fc6ef
hxexxn <102952683+hxexxn@users.noreply.github.com>
2022-09-15 16:05:22 +0900

    First Commit

---
+++ src/test/java/com/frogame/www/FrogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogame.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
