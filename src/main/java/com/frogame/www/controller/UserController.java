8c4251d291471ff830ef740df12341926bffddc4
hxexxn <102952683+hxexxn@users.noreply.github.com>
2022-09-15 23:20:51 +0900

    Thymeleaf Layout 설정
    Thymeleaf Layout (fragment) 페이지 테스트 및 초기 설정 완료

--- src/main/java/com/frogame/www/controller/UserController.java
+++ src/main/java/com/frogame/www/controller/UserController.java
@@ -1,7 +1,27 @@
 package com.frogame.www.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping (value = "/user")
 public class UserController {
+
+    @GetMapping (value = "/test")
+    public String test(Model model) {
+        model.addAttribute("data", "타임리프 예제입니다.");
+        return "user/userTest";
+    }
+
+    @GetMapping (value = "login")
+    public String user_login() {
+        return "user/user_login";
+    }
+
+    @GetMapping (value = "join")
+    public String user_join() {
+        return "user/user_join";
+    }
 }
